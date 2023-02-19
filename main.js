7a11dca19b8e15a9146ae28647d601c80eb85788
lzfilms5 <lzfilms4@gmail.com>
2023-02-19 14:43:03 +0700

    asdasd

--- main.js
+++ main.js
@@ -3,22 +3,21 @@ const mongoose = require('mongoose');
 const express = require('express');
 const Router = require('./Router')
 const PORT = process.env.PORT || 5000
+mongoose.set('strictQuery', false);
+mongoose
+    .connect('mongodb+srv://TIFTEL:5TgM4aVoo@cluster0.c06a4qf.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('ok')
+    })
+    .catch(err => {
+        console.log(err)
+    })
 const app = express()
-
 app.use(express.json())
-app.use("/Router", Router)
-const start = async () =>{
-    try {
-        await mongoose.connect('mongodb+srv://tysachnyk1111:5TgM4aVoo@cluster0.wtrbjof.mongodb.net/?retryWrites=true&w=majority')
-        app.listen(PORT, ()=> console.log('server started on port ${PORT}'))
-    }catch(e){
-        console.log(e)
-    }
-}
 
-start()
-
-//app.get('/', async (req,res)=>{
-    //const  = await postModel.find({})
-        //res.send(POSTS);
-//})
+app.get('/', (req,res)=>{
+        res.send('asd');
+})
+app.listen(PORT, () => {
+    console.log('Server OK');
+  });
