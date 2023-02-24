b0053a502567691036d3b1216d00bc5d2d49a546
Tarasov_Georgiy <99883221+TIFTEL@users.noreply.github.com>
2023-02-24 14:40:47 +0700

    ez

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 
 const mongoose = require('mongoose');
 const express = require('express');
-const Router = require('./Router')
 const PORT = process.env.PORT || 5000
-
+const UserModel = require('./Users')
 mongoose
     .connect('mongodb+srv://TIFTEL:5TgM4aVoo@cluster0.c06a4qf.mongodb.net/blog?retryWrites=true&w=majority')
     .then(() => {
@@ -14,10 +13,28 @@ mongoose
     })
 const app = express()
 app.use(express.json())
+app.post('/auth/123', async (req, res)=>{
+    try{
+        const user = await UserModel.findOne({ fullName: req.body.fullName})
 
-app.get('/', (req,res)=>{
+        if(!user){
+            return res.status(400).json({
+                message:'Пользователь не найден'
+            });
+        }
+        const {...userdata}=user._doc;
+        res.json({
+            ...userdata,
+        });
+    } catch (err){
+
+    }
+})
+app.get('/auth/12345', (req,res)=>{
     res.send('asd');
 })
 app.listen(PORT, () => {
     console.log('Server OK');
 });
+
+//post and get запросы, три поля с тестами настроением и предполагаемый уровень счастья(нейронка)
