82c729a5d53e93549b3e934e67ab36eee213c6e2
Tarasov_Georgiy <99883221+TIFTEL@users.noreply.github.com>
2023-02-26 13:09:33 +0700

    23434

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ const express = require('express');
 const PORT = process.env.PORT || 5000
 const UserModel = require('./Users')
 const catboost = require("catboost");
-var fs = require("fs");
+const fs = require("fs");
+const bodyParser = require('body-parser');
+
 mongoose
     .connect('mongodb+srv://TIFTEL:5TgM4aVoo@cluster0.c06a4qf.mongodb.net/blog?retryWrites=true&w=majority')
     .then(() => {
@@ -13,25 +15,25 @@ mongoose
     .catch(err => {
         console.log(err)
     })
+
 const app = express()
-app.get('/listUsers', function (req, res) {
-    fs.readFile( UserModel + "/" + "Users.json", 'utf8', function (err, data) {
-        console.log( data );
-        res.end( data );
-    });
-})
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 app.use(express.json())
-app.delete('/deleteUser', function (req, res) {
 
 
-    fs.readFile( UserModel + "/" + "users.json", 'utf8', function (err, data) {
-        data = JSON.parse( data );
-        delete data["UserModel"];
+app.delete('/posts/:id', (req, res) => {
+    UserModel.remove({
+        _id: req.params.id
+    }).then(post => {
+        if(post) {
+            res.json({ status: "deleted"});
+        } else {
+            res.json({ status: "error"});
+        }
+    })
+})
 
-        console.log( data );
-        res.end( JSON.stringify(data));
-    });
-}),
     app.get('/:id', function (req, res) {
         // First read existing users.
         fs.readFile( UserModel + "/" + "users.json", 'utf8', function (err, data) {
@@ -41,28 +43,33 @@ app.delete('/deleteUser', function (req, res) {
             res.end( JSON.stringify(user));
         });
     }),
-app.post('/auth/123', async (req, res)=>{
-    try{
 
-        const user = await UserModel.findOne({ fullName: req.body.fullName})
+app.post('/auth/123', async (req, res) => {
+        try {
+            const data = req.body;
 
-        const {...userdata}=user._doc;
-        res.json({
-            ...userdata,
-        });
-    } catch (err){
+            console.log(data);
+            const doc = new UserModel({
+                fullName: req.body.fullName,
+                Happiness: req.body.Happiness
+            });
 
-    }
-})
+            const user = await doc.save();
+
+        }catch (err) {
+
+        }
+    })
+
 model = new catboost.Model();
 model.loadModel('modeltest.cbm');
 app.get('/', (req,res)=>{
     prediction = model.predict(
-        [[36, 1, 599, 1, 3, 3, 1, 42, 3, 1, 1, 2596, 1, 1, 80, 10, 10, 0, 7, 8]],
+        [[36, 2, 599, 1, 3, 3, 1, 42, 3, 1, 1, 25960, 1, 1, 80, 10, 10, 0, 7, 8]],
         [['Age','BusinessTravel','DailyRate','Department','Education','EducationField','Gender','HourlyRate','JobInvolvement','JobLevel','MaritalStatus','MonthlyIncome','NumCompaniesWorked','OverTime','StandardHours','TotalWorkingYears','YearsAtCompany','YearsInCurrentRole','YearsSinceLastPromotion','YearsWithCurrManager']]);
     res.send(prediction);
-
 })
+
 app.listen(PORT, () => {
     console.log('Server OK');
 });
