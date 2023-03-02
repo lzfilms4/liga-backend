d2ea56a49448c55e57ad6e9fcce65145792f104c
lzfilms5 <lzfilms4@gmail.com>
2023-03-02 11:18:09 +0700

    1.1.1.

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
- });
+ });   
 app.post('/person/create', async (req, res) => {
   // const mlResult = axios.get('https://flask-production-a780.up.railway.app/', {
   //   age: req.body.age,
